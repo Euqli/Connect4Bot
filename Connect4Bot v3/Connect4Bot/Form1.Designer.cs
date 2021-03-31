c5023b6fe1142b17497381ec140e714acc780d54
Euqli <81697187+Euqli@users.noreply.github.com>
2021-03-31 16:32:48 +0200

    Add files via upload

---
+++ Connect4Bot v3/Connect4Bot/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Connect4Bot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectFourAdapter1 = new Connect_Four_Adapter.ConnectFourAdapter();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // connectFourAdapter1
+            // 
+            this.connectFourAdapter1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.connectFourAdapter1.Location = new System.Drawing.Point(12, 12);
+            this.connectFourAdapter1.MinimumSize = new System.Drawing.Size(830, 560);
+            this.connectFourAdapter1.Name = "connectFourAdapter1";
+            this.connectFourAdapter1.Password = "";
+            this.connectFourAdapter1.Size = new System.Drawing.Size(830, 560);
+            this.connectFourAdapter1.TabIndex = 0;
+            this.connectFourAdapter1.Username = "";
+            this.connectFourAdapter1.OnYourMove += new Connect_Four_Adapter.YourMoveEventArgsDelegate(this.connectFourAdapter1_OnYourMove);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(848, 258);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(166, 318);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(851, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MAX_DEPTH";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(854, 56);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(70, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 2;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown2.Location = new System.Drawing.Point(944, 56);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(70, 20);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            65536});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(848, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 20);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "static";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(869, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Static";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(954, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Dynamic";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 588);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.connectFourAdapter1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Connect_Four_Adapter.ConnectFourAdapter connectFourAdapter1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Connect4Bot v3/Connect4Bot/Form1.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connect4Bot
+{
+    public partial class Form1 : Form
+    {
+        readonly static int NUM_OF_COLUMNS = 7;
+        readonly static int NUM_OF_ROWS = 6;
+        readonly static int LAST_ROW_IND = NUM_OF_ROWS - 1;
+
+        static int MAX_DEPTH = 9; // Maximum search depth
+        static bool DO_STATIC_DEPTH = true;
+
+        static double experimentalVariable;
+        static double[] ROW_WORTH = new double[] {
+            6.1, 4.1, 2.8, 2.0, 1.5, 1.2, 1.0
+        }; // 0.2 * 1.6 ** x + 0.7
+
+        readonly static int INFINITY = 100000000; // 100 000 000
+
+        readonly static int CON_4_VAL = 10000; // 10 000
+        readonly static int CON_3_VAL = 50;
+
+        readonly static int BOT_COLOR = 1;
+        readonly static int BOT_VALUE = CON_4_VAL;
+        readonly static int FOE_COLOR = 2;
+        readonly static int FOE_VALUE = -100 * CON_4_VAL;
+
+        readonly static int[] COL_SEARCH_ORDER = { 
+            3, 2, 4, 1, 5, 0, 6 
+        };
+
+        readonly static int[,] NUM_OF_POT_WINS = new int[,] {
+            { 3,  4,  5,  5,  4,  3 },
+            { 4,  6,  8,  8,  6,  4 },
+            { 5,  8,  11, 11, 8,  5 },
+            { 7,  10, 13, 13, 10, 7 },
+            { 5,  8,  11, 11, 8,  5 },
+            { 4,  6,  8,  8,  6,  4 },
+            { 3,  4,  5,  5,  4,  3 }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void connectFourAdapter1_OnYourMove(Connect_Four_Adapter.YourMoveEventArgs e)
+        {
+            int[,] board = connectFourAdapter1.GetBoard();
+
+            PrintBoard(board);
+
+            int move = MainSearchAlgorithm(board);
+            connectFourAdapter1.SendMove(move);
+        }
+
+        // Searches top-layer and returns the move
+        static int MainSearchAlgorithm(int[,] board)
+        {
+            int row;                    // temp row
+            int evalMaxIndex = -1;      // col-index to return
+            int eval = -1 * INFINITY;   // evaluation value at col-index
+            int alpha = -1 * INFINITY;  // MIN guaranteed value for BOT (botten kommer kunna få ett värde större än detta)
+            int beta = INFINITY;        // MAX guaranteed value for FOE
+
+            // For diagnostics
+            int[] evalArr = new int[7];
+            bool[] evalArrChanged = new bool[7];
+
+            // TODO: Order moves acc to num of threats
+
+            // TODO: Set minimal depth in begining and maximal in end.
+            if (!DO_STATIC_DEPTH)
+                MAX_DEPTH = FindOptimalDepth(board);
+
+            System.Diagnostics.Debug.Write("MAX_DEPTH: ");
+            System.Diagnostics.Debug.WriteLine(MAX_DEPTH);
+
+            // Tests to put one in each column
+            //for (int col = 0; col < 7; col++)
+            foreach (int col in COL_SEARCH_ORDER)
+            {
+                row = ColToPos(col, board); // Grabs available y-coord
+                if (row == -1)
+                    continue;
+
+                board[col, row] = BOT_COLOR; // Sets pos in board to BOT_COLOR
+                int evalTemp = Minimax(board, MAX_DEPTH, alpha, beta, false); // Uses recursive minmax to evaluate pos
+                board[col, row] = 0; // Resets pos
+
+                evalArr[col] = evalTemp; // For diagnostics
+                evalArrChanged[col] = true;
+
+                if (evalTemp > eval)
+                {
+                    evalMaxIndex = col;
+                    eval = evalTemp;
+                }
+            }
+
+            for (int col = 0; col < 7; col++)
+            {
+                if (evalArrChanged[col])
+                    System.Diagnostics.Debug.WriteLine("Column " + col + ": " + evalArr[col]);
+                else
+                    System.Diagnostics.Debug.WriteLine("Column " + col + ": None");
+            }
+            System.Diagnostics.Debug.WriteLine("");
+
+            return evalMaxIndex;
+        }
+
+        // Finds optimal depth to search to not take to long.
+        static int FindOptimalDepth(int[,] board)
+        {
+            double resDepth = 0;
+
+            int temp = 0; // may be 1 with other met
+            
+            //int[] colArray = new int[NUM_OF_COLUMNS];
+            for (int col = 0; col < NUM_OF_COLUMNS; col++)
+            {
+                for (int row = 0; row < NUM_OF_ROWS; row++)
+                {
+                    if (board[col, row] != 0)
+                        break;
+                    ++temp;
+                    //++colArray[col];
+                    //colArray[col] += NUM_OF_POT_WINS[col, row];
+                }
+                //resDepth += Math.Pow(experimentalVariable, colArray[col] - 2) - Math.Pow(experimentalVariable, 4) + 1; // 0.67 ** (x-2) + 1 - 0.67 ** 4
+                //temp *= colArray[col];
+            }
+
+            if (temp < 16)
+                resDepth = 16;
+            else if (temp < 26)
+                resDepth = 10;
+            else if (temp < 36)
+                resDepth = 9;
+            else
+                resDepth = 7;
+
+            return Convert.ToInt32(resDepth); // ToInt32 rounds double to the nearest int
+        }
+
+        // Recursive function with alpha-beta till  evaluate
+        static int Minimax(int[,] board, int depth, int alpha, int beta, bool playerMax)
+        {
+            if (depth == 0 || BoardIsFull(board) || GetWinner(board) != 0)
+                return PartialEvaluation(board);
+
+            int row;
+
+            if (playerMax)
+            {
+                int eval = -1 * INFINITY;
+                //for (int col = 0; col < 7; col++)
+                foreach (int col in COL_SEARCH_ORDER)
+                {
+                    row = ColToPos(col, board);
+                    if (row == -1)
+                        continue;
+                    board[col, row] = BOT_COLOR;
+                    eval = Math.Max(eval, Minimax(board, depth - 1, alpha, beta, false));
+                    board[col, row] = 0;
+                    alpha = Math.Max(alpha, eval);
+                    if (alpha >= beta)
+                        break;
+                }
+                return eval;
+            }
+            else
+            {
+                int eval = INFINITY;
+                //for (int col = 0; col < 7; col++)
+                foreach (int col in COL_SEARCH_ORDER)
+                {
+                    row = ColToPos(col, board);
+                    if (row == -1)
+                        continue;
+                    board[col, row] = FOE_COLOR;
+                    int evalTemp = Minimax(board, depth - 1, alpha, beta, true);
+                    board[col, row] = 0;
+                    eval = Math.Min(evalTemp, eval);
+                    beta = Math.Min(beta, eval);
+                    if (alpha >= beta)
+                        break;
+                }
+                return eval;
+            }
+        }
+
+        // Approximates the value of a board with a positive
+        // value beeing in favour of the bot and negative the foe.
+        static int PartialEvaluation(int[,] board)
+        {
+            int res = 0;
+
+            /*
+            for (int col = 0; col < NUM_OF_COLUMNS; col++)
+                for (int row = 0; row < NUM_OF_ROWS; row++)
+                {
+                    if (board[col, row] == BOT_COLOR)
+                        res += NUM_OF_POT_WINS[col, row];
+                    else if (board[col, row] != 0)
+                        res -= NUM_OF_POT_WINS[col, row];
+                }
+            */
+
+            res += ColorToWeightedScore(GetWinner(board));
+
+            res += ValueOf3Connected(board) * CON_3_VAL;
+
+            return res;
+        }
+
+        // Returns first instance of four-in-a-row.
+        static int GetWinner(int[,] board)
+        {
+            // Kolla horrisontellt  
+            for (int r = 0; r < NUM_OF_ROWS; r++)
+                for (int c = 0; c < NUM_OF_COLUMNS - 3; c++)
+                    if (board[c, r] != 0 && board[c, r] == board[c + 1, r] && board[c + 1, r] == board[c + 2, r] && board[c + 2, r] == board[c + 3, r])
+                        return board[c, r];
+
+            // Kolla vertikalt  
+            for (int r = 0; r < NUM_OF_ROWS - 3; r++)
+                for (int c = 0; c < NUM_OF_COLUMNS; c++)
+                    if (board[c, r] != 0 && board[c, r] == board[c, r + 1] && board[c, r + 1] == board[c, r + 2] && board[c, r + 2] == board[c, r + 3])
+                        return board[c, r];
+
+            //Kolla diagonalt  
+            for (int r = 0; r < NUM_OF_ROWS - 3; r++)
+                for (int c = 0; c < NUM_OF_COLUMNS - 3; c++)
+                {
+                    if (board[c, r] != 0 && board[c, r] == board[c + 1, r + 1] && board[c + 1, r + 1] == board[c + 2, r + 2] && board[c + 2, r + 2] == board[c + 3, r + 3])
+                        return board[c, r]; // upp  
+                    if (board[c + 3, r] != 0 && board[c + 3, r] == board[c + 2, r + 1] && board[c + 2, r + 1] == board[c + 1, r + 2] && board[c + 1, r + 2] == board[c, r + 3])
+                        return board[c + 3, r]; // ned  
+                }
+
+            return 0;
+        }
+
+        // Returns number of three-in-a-rows for bot minus foe.
+        // Uses raw score.
+        static int ValueOf3Connected(int[,] board)
+        {
+            int res = 0;
+
+            // Check horizontally  
+            for (int r = 0; r < NUM_OF_ROWS; r++)
+                for (int c = 0; c < NUM_OF_COLUMNS - 2; c++)
+                    if (board[c, r] != 0 && board[c, r] == board[c + 1, r] && board[c + 1, r] == board[c + 2, r])
+                        res += ColorToRawScore(board[c, r]);
+
+            // Check vertically  
+            for (int r = 0; r < NUM_OF_ROWS - 2; r++)
+                for (int c = 0; c < NUM_OF_COLUMNS; c++)
+                    if (board[c, r] != 0 && board[c, r] == board[c, r + 1] && board[c, r + 1] == board[c, r + 2])
+                        res += ColorToRawScore(board[c, r]);
+
+            // Check diagonally 
+            for (int r = 0; r < NUM_OF_ROWS - 2; r++)
+                for (int c = 0; c < NUM_OF_COLUMNS - 2; c++)
+                {
+                    if (board[c, r] != 0 && board[c, r] == board[c + 1, r + 1] && board[c + 1, r + 1] == board[c + 2, r + 2])
+                        res += ColorToRawScore(board[c, r]); // down  
+                    if (board[c + 2, r] != 0 && board[c + 2, r] == board[c + 1, r + 1] && board[c + 1, r + 1] == board[c, r + 2])
+                        res += ColorToRawScore(board[c + 2, r]); // up  
+                }
+
+            return res;
+        }
+
+        // Returns value of colour from the perspective of bot.
+        // FOE's score is piroritized. 
+        static int ColorToWeightedScore(int color)
+        {
+            if (color == BOT_COLOR)
+                return BOT_VALUE;
+            else if (color == FOE_COLOR)
+                return FOE_VALUE;
+            else
+                return 0;
+        }
+
+        // Returns value of colour from the perspective of bot.
+        // FOE's score is as important as the BOT's score. 
+        static int ColorToRawScore(int color)
+        {
+            if (color == BOT_COLOR)
+                return 1;
+            else if (color == FOE_COLOR)
+                return -1;
+            else
+                return 0;
+        }
+
+        // Looks up empty position in column from bottom up
+        // and returns row number, or if full returns -1.
+        static int ColToPos(int col, int[,] board)
+        {
+            for (int row = LAST_ROW_IND; row != -1; row--)
+                if (board[col, row] == 0)
+                    return row;
+            return -1;
+        }
+
+        // Returns true there are no empty spots in top row.
+        static bool BoardIsFull(int[,] board)
+        {
+            for (int col = 0; col < NUM_OF_COLUMNS; col++)
+                if (board[col, 0] == 0)
+                    return false;
+            return true;
+        }
+
+        // Returns true there are only empty spots in bottom row.
+        static bool BoardIsEmpty(int[,] board)
+        {
+            for (int col = 0; col < NUM_OF_COLUMNS; col++)
+                if (board[col, LAST_ROW_IND] != 0)
+                    return false;
+            return true;
+        }
+
+        // Writes board to the debug console.
+        static void PrintBoard(int[,] board)
+        {
+            for (int r = 0; r < NUM_OF_ROWS; r++)
+            {
+                for (int c = 0; c < NUM_OF_COLUMNS; c++)
+                    System.Diagnostics.Debug.Write(board[c, r]);
+                System.Diagnostics.Debug.WriteLine("");
+            }
+
+            System.Diagnostics.Debug.WriteLine("");
+        }
+
+        // Not in use.
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.BackColor = Color.White;
+            numericUpDown2.BackColor = Color.Gray;
+
+            MAX_DEPTH = Convert.ToInt32(numericUpDown1.Value); //Must remove readonly from MAX_DEPTH 
+            DO_STATIC_DEPTH = true;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.BackColor = Color.Gray;
+            numericUpDown2.BackColor = Color.White;
+
+            experimentalVariable = Decimal.ToDouble(numericUpDown2.Value);
+            DO_STATIC_DEPTH = false;
+        }
+    }
+}
